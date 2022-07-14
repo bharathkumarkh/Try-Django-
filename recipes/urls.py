16f35b2a2e47edbced60c25a52d628e5fe675671
Bharath Kumar <khbharathofficial@gmail.com>
2022-07-14 19:11:11 +0530

    Completed 68

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,8 @@ from .views import (
     recipe_detail_view,
     recipe_create_view,
     recipe_update_view,
-    recipe_detail_hx_view
+    recipe_detail_hx_view,
+    recipe_ingredient_update_hx_view
 )
 
 app_name='recipes'
@@ -14,6 +15,8 @@ urlpatterns = [
     path("", recipe_list_view,name='list'),
     path("create/", recipe_create_view,name='create'),
     path("hx/<int:id>/", recipe_detail_hx_view,name='hx-detail'),
+    path("hx/<int:parent_id>/ingredient/<int:id>/", recipe_ingredient_update_hx_view,name='hx-ingredient-detail'),
+    path("hx/<int:parent_id>/ingredient/", recipe_ingredient_update_hx_view,name='hx-ingredient-create'),
     path("<int:id>/edit/", recipe_update_view,name='update'),
     path("<int:id>/", recipe_detail_view,name='detail'),
     
