0fb4994a5ce3923a4e5097e354620563da5f7799
Bharath Kumar <khbharathofficial@gmail.com>
2022-07-16 17:41:06 +0530

    Slight Changes End of 75

--- trydjango/cdn/conf.py
+++ trydjango/cdn/conf.py
@@ -2,12 +2,12 @@ import os
 
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "trydjangoo"
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL= "https://sgp1.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS={
     "CacheControl":"max-age=86400",
 }
-AWS_LOCATION = "https://trydjangoo.sgp1.digitaloceanspaces.com"
+AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.sgp1.digitaloceanspaces.com"
 
 DEFAULT_FILE_STORAGE = "trydjango.cdn.backends.MediaRootS3Boto3Storage"
 STATICFILES_STORAGE = "trydjango.cdn.backends.StaticRootS3Boto3Storage"
